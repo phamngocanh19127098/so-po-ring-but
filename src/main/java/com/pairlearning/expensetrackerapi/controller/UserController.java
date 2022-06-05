badf2566eb37ecc99dfcc0f428911f16d52f8b8a
phamngocanh19127098 <npham4533@gmail.com>
2022-06-05 18:48:00 +0700

    first commit

---
+++ src/main/java/com/pairlearning/expensetrackerapi/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.pairlearning.expensetrackerapi.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody Map<String,Object> userMap){
+        String firstName = (String) userMap.get("firstName");
+        String lastName = (String) userMap.get("lastName");
+        String email = (String) userMap.get("email");
+        String password = (String) userMap.get("password");
+        return firstName + ", "+ lastName + ", "+ email+ ", "+ password;
+    }
+
+    @GetMapping("")
+    public String hello(){
+        return "Hello World";
+    }
+}
